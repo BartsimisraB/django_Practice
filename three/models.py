30b65a103a6a9761f22d1ebdf96e2e96823fbd5d
kunyoung <rlarjsdyd3@gmail.com>
2023-04-07 15:18:39 +0900

    04-07

--- three/models.py
+++ three/models.py
@@ -1,31 +1,23 @@
 from django.db import models
 
-# DB에서 테이블의 역할을 하는 클래스 작성 파일
-# model을 설정하는 파일
-
 class Food(models.Model):
-    name = models.CharField(max_length=20)
-    adr = models.CharField(max_length=50)
-    # 비밀번호를 설정하고 싶은 경우 - 길이 20자, 공백 비허용
-    pw = models.CharField(max_length=20, default=None, null=True)
+    name=models.CharField(max_length=20)
+    adr=models.CharField(max_length=50)
+    pw=models.CharField(max_length=20,default=None,null=True)
 
-    create_day = models.DateTimeField(auto_now_add=True)
-    update_day = models.DateTimeField(auto_now=True)
-
+    create_day=models.DateTimeField(auto_now_add=True)
+    update_day=models.DateTimeField(auto_now=True)
 
 class Review(models.Model):
-    score = models.IntegerField()
-    comment = models.CharField(max_length=100)
+    score=models.IntegerField()
+    comment=models.CharField(max_length=100)
 
-    # Food가 지워진다면 on_delete로 CASCADE(종속)된 Review도 같이 지워진다.
-    # Food : Review >> one to many
-    res = models.ForeignKey(Food, on_delete=models.CASCADE)
-
-    # Food가 지워져도 아무런 일도 하지 않는다.
-    # res = models.ForeignKey(Food, on_delete=models.DO_NOTHING)
-
-    # Food가 지워지면 Food 속성이 NULL 값으로 채워진다.
-    # res = models.ForeignKey(Food, on_delete=models.SET_NULL())
-
+    res=models.ForeignKey(Food,on_delete=models.CASCADE)
     create_day = models.DateTimeField(auto_now_add=True)
     update_day = models.DateTimeField(auto_now=True)
+
+
+
+
+
+
--- three/views.py
+++ three/views.py
@@ -1,90 +1,88 @@
-from django.shortcuts import render, get_object_or_404, redirect  # get_object_or_404 로 에러 페이지 변경
-from three.models import Food, Review  # models 가 DB를 의미
+from django.shortcuts import render, get_object_or_404, redirect
+from three.models import Food, Review
 from django.core.paginator import Paginator
-from three.forms import FoodForm, ReviewForm
+from three.forms import FoodForm, ReviewForm, UpdateFoodForm
 from django.http import HttpResponseRedirect
+from django.db.models import Count,Avg
 
+def li(request):
+    res=Food.objects.all().annotate(reviews_count=Count('review')).annotate(average_score=Avg('review__score'))
+    paginator=Paginator(res,5) #한 페이지당 5개씩 표시
+    page=request.GET.get('page')
+    a=paginator.get_page(page)
 
-def li(reqeust):
-    res = Food.objects.all()
-    paginator = Paginator(res, 5)  # 한 페이지당 5개
-    page = reqeust.GET.get('page')
-    a = paginator.get_page(page)
+    con={'res':a}
+    return render(request, 'three/li.html', con)
 
-    con = {'res': a}
-    return render(reqeust, 'three/li.html', con)
-
-
+#p.113
 def create(request):
-    if request.method == 'POST':
-        form = FoodForm(request.POST)
+    if request.method=='POST':
+        form=FoodForm(request.POST)
         if form.is_valid():
-            a = form.save()
+            a=form.save()
         return HttpResponseRedirect('/three/li/')
-    form = FoodForm()
-    return render(request, 'three/create.html', {'form': form})
-
+    form=FoodForm()
+    return render(request,'three/create.html',{'form':form})
 
 def update(request):
-    if request.method == 'POST' and 'id' in request.POST:
-        # a = Food.objects.get(pk=request.POST.get('id'))
-        a = get_object_or_404(Food, pk=request.POST.get('id'))  # 에러 발생시 404 페이지가 나오게 만듬
-        pw = request.POST.get('pw', "")
-        form = FoodForm(request.POST, instance=a)  # 수정할 대상 객체 지정
+    if request.method=='POST' and 'id' in request.POST:
+        a=get_object_or_404(Food, pk=request.POST.get('id'))
+        pw=request.POST.get('pw',"")
+        form=UpdateFoodForm(request.POST, instance=a)
+        if form.is_valid() and pw==a.pw:
+            a=form.save()
+    elif 'id' in request.GET:
+        a=get_object_or_404(Food, pk=request.GET.get('id'))
+        form=FoodForm(instance=a)
+        form.pw=''
+        return render(request,'three/update.html',{'form':form})
+    return HttpResponseRedirect('/three/li/')
 
-        if form.is_valid() and pw == a.pw:
-            a = form.save()  # 수정한 데이터를 테이블에 저장
-
-    elif 'id' in request.GET:  # 데이터를 가져올 때는 GET 방식
-        # a = Food.objects.get(pk=request.GET.get('id'))
-        a = get_object_or_404(Food, pk=request.GET.get('id'))
-        form = FoodForm(instance=a)  # 수정된 데이터 저장
-        return render(request, 'three/update.html', {'form': form})  # 가져온 데이터를 해당 url로 넘김
-
+#id(pk)를 url 파라미터를 통해 전달받는다
+def read(request,id):
+    if id is not None:
+        a=get_object_or_404(Food, pk=id)
+        re=Review.objects.filter(res=a).all()
+        return render(request, 'three/read.html', {'a':a, 're': re})
     return HttpResponseRedirect('/three/li')
 
-    # def read(request):
-    #     if 'id' in request.GET:  # id 값이 존재하는지 확인
-    #         a = get_object_or_404(Food, pk=request.GET.get('id'))
-    #         return render(request, 'three/read.html', {'a': a})
-    #     return HttpResponseRedirect("/three/li")
+def delete(request,id):
+    a = get_object_or_404(Food, pk=id)
+    if request.method =='POST' and 'pw' in request.POST:
+        if a.pw==request.POST.get('pw'):
+            a.delete()
+            return redirect('li')
+        return redirect('res-read',id=id)
+    return render(request,'three/delete.html',{'a':a})
 
 
-# id (pk) 를 url 파라미터를 통해 전달받는다.
-def read(request, id):  # urls.py 에서 작성한 <int:id>로 받은 파라미터를 사용하는 방식
-
-    if id is not None:  # id 값이 존재하는지 확인
-        a = get_object_or_404(Food, pk=id)
-        re = Review.objects.filter(res=a).all()    # 음식에 대한 리뷰를 전부 들고와서 re로 담아 보낸다
-        return render(request, 'three/read.html', {'a': a, 're': re})
-    return HttpResponseRedirect("/three/li")
-
-#
 
 
-def delete(request):
-    if 'id' in request.GET:
-        a = get_object_or_404(Food, pk=request.GET.get('id'))
-        a.delete()
-    return HttpResponseRedirect('/three/li')
-
-
-def review_create(request, food_id):
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
+def review_create(request,food_id):
+    if request.method=='POST':
+        form=ReviewForm(request.POST)
         if form.is_valid():
-            a = form.save()
-        # HttpResponseRedirect와 같은 의미
-        # redirect는 view name을 기반으로 움직인다. (조금 더 간편함)
-        # url이 변경되도 view name 이 그대로면 변경하지 않아도 된다.
-        return redirect('res-read', id=food_id)
+            a=form.save()
+        return redirect('res-read',id=food_id)
+        #view name으로 움직이기 때문에 url이 변경되도 view name이 그대로면 변경하지않아도됨
 
-    a = get_object_or_404(Food, pk=food_id)
-    form = ReviewForm(initial={'res': a})
-    return render(request, 'three/review_create.html', {'form': form, 'a': a})
+    a=get_object_or_404(Food,pk=food_id)
+    form=ReviewForm(initial={'res':a})
+    return render(request, 'three/review_create.html',{'form':form, 'a':a})
 
-
-def review_delete(request, food_id, review_id):
-    a = get_object_or_404(Review, pk=review_id)
+def review_delete(request,food_id,review_id):
+    a=get_object_or_404(Review,pk=review_id)
     a.delete()
-    return redirect('res-read', id=food_id)
+    return redirect('res-read',id=food_id)
+
+def review_li(request):
+    reviews=Review.objects.all().select_related()
+    paginator=Paginator(reviews, 10)
+
+    page=request.GET.get('page')
+    a=paginator.get_page(page)
+
+    con={'reviews':a}
+    return render(request,'three/review_li.html',con)
+
+
